f06d8a1c1ab8df9ae3d84319e082a7c5b0a65121
Ali <aliannea.sherman@gmail.com>
2023-10-15 00:53:32 -0700

    first commit

---
+++ src/main/java/com/ali/fun/funtimes/FuntimesApplication.java
@@ -0,0 +1,13 @@
+package com.ali.fun.funtimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuntimesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuntimesApplication.class, args);
+	}
+
+}
